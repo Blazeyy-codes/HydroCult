285cbf6ffc4bde0fa87bc084f56c9230bff387a9
Baibhav Shrestha <baibhavstha77@gmail.com>
2026-01-05 21:36:29 +0000

    so i have successfully deployed now lets get to adding the environmental

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
+// This file is being kept for backwards compatibility.
+// New projects should use environment variables and the initializeApp() function without arguments.
+// For more information, see: https://firebase.google.com/docs/hosting/reserved-urls#sdk-initialization
+
 export const firebaseConfig = {
-  "projectId": "studio-9392948812-a37d2",
-  "appId": "1:462875365818:web:eee992cf124e01d3220d83",
-  "apiKey": "AIzaSyDk6kzjRvA0wZ2thd6bYZetjp18q6RGIO8",
-  "authDomain": "studio-9392948812-a37d2.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "462875365818"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
